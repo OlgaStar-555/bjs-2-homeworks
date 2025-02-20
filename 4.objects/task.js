3dc071bcc4bebaeafc1393e7be60c868d272f077
Olga Starikova <starikovaolga555@gmail.com>
2025-02-20 22:39:01 +0300

    task #4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+    this.name = name
+    this.gender = gender
+    this.age = age
+    this.marks = []
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.marks) {
+        this.marks.push(...marks)
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.marks?.length) {
+        return this.marks.reduce((accumulator, currentValue) => accumulator + currentValue) / this.marks.length;
+    } else {
+        return 0
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject
+    delete this.marks
+
+    this.excluded = reason
 }
+
+const student0 = new Student('Ivan', 'мужской', 25)
+const student1 = new Student('Anna', 'женский', 19)
+const student2 = new Student('Kesha', 'мужской', 22)
+
